85736fd53efa5639fe69d0ac93116d5223776b15
samiya-maqsood <139143569+samiya-maqsood@users.noreply.github.com>
2023-07-30 22:29:08 +0200

    Update Plant_Phenotyping.R

--- Plant_Phenotyping.R
+++ Plant_Phenotyping.R
@@ -32,14 +32,14 @@ ggplot() +
   labs(x = "Group", y = "Plant Weight", title = "Plant Growth by Group") +
   theme_minimal() +
   theme(legend.position = "none") +
-  geom_text(data = group_summary, aes(x = group, y = MeanWeight + 0.5,
+  geom_text(data = group_summary, aes(x = group, y = MeanWeight + 0.05,
                                       label = paste("Mean:", round(MeanWeight, 2))),
             position = position_dodge(width = 0.8), vjust = -0.5, size = 4) +
   geom_errorbar(data = group_summary, aes(x = group, ymin = MeanWeight - SDWeight, ymax = MeanWeight + SDWeight),
                 position = position_dodge(width = 0.8), width = 0.25) +
   geom_hline(data = group_summary, aes(yintercept = MedianWeight),
              linetype = "dashed", color = "blue") +
-  geom_text(data = group_summary, aes(x = group, y = MedianWeight - 0.5,
+  geom_text(data = group_summary, aes(x = group, y = MedianWeight - 0.05,
                                       label = paste("Median:", round(MedianWeight, 2))),
             position = position_dodge(width = 0.8), vjust = 1.5, color = "blue", size = 4)
 
